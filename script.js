1a2221afdff9810c5596d5e1b62e7ffaca0cc840
priyanka9913 <rathipriyanka1399@gmail.com>
2025-03-06 23:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const dropDown = document.getElementById("colorSelect")
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+    const selectedOption = dropDown.options[dropDown.selectedIndex]
+    dropDown.removeChild(selectedOption);
+})
